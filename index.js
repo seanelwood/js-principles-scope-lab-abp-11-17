3662457b28d0b2b345564167405bfebd5a152817
Sean Elwood <seanelwood@gmail.com>
2017-11-09 18:23:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Write your solution in this file!
 var customerName = "bob";
 
-function upperCaseCustomerName(x) {
-  return x.toUpperCase();
+function upperCaseCustomerName() {
+  return customerName.toUpperCase();
 }
 upperCaseCustomerName(customerName);
