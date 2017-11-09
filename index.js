24d4f6e88ea69a6524601c51f4380c776cf9d8e1
Sean Elwood <seanelwood@gmail.com>
2017-11-09 18:24:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@
 var customerName = "bob";
 
 function upperCaseCustomerName() {
-  return customerName.toUpperCase();
+  return var customerName.toUpperCase();
 }
 upperCaseCustomerName(customerName);
